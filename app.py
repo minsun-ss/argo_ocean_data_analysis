f5f8cd9813808e977c3c8a56bdcfd45d2f84d561
minsun-ss <sungsharon@gmail.com>
2020-09-13 18:12:39 -0400

    Moved visualization folder into assets since dash demands locals to be stored there.

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import json
 import pandas as pd
 from helper import config as cfg
 
+gulf_geojson = json.load(open('assets/iho.json'))
+
 # Note that static assets such as html and the like must be served from the asset folders because Dash is pain
 app = dash.Dash(__name__)
 app.title = 'Ocean Temperature and Salinity in the Estuary and Gulf of St. Lawrence'
@@ -23,7 +25,21 @@ app.layout = html.Div([
 )
 def update_figure(selected):
     return {
-        'layout': go.Layout(autosize=True, hovermode='closest', height=600)
+        'data': [
+            go.Scattermapbox(
+                lat=[45.659196],
+                lon=[-66.196285],
+                mode='markers',
+                marker=dict(size=4)
+            )
+        ],
+        'layout': go.Layout(autosize=True, hovermode='closest', height=900
+        , mapbox = {'accesstoken': cfg.MAPBOX_TOKEN, 'bearing': 0, 'layers':
+                [dict(sourcetype='geojson', source=gulf_geojson, type='fill', color = 'rgba(208,28,139,0.8)')]
+                    , 'center': {'lat': 48, 'lon': -61}, 'zoom': 5
+                    , 'style': 'mapbox://styles/mapbox/light-v9'
+                    }
+        )
     }
 
 if __name__== '__main__':
