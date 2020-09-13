f77f9116fc48a311422557e5478ed3815a0de417
CICarlier <35584629+CICarlier@users.noreply.github.com>
2020-09-13 19:58:41 -0400

    added aws key

--- helper/config.py
+++ helper/config.py
@@ -1,3 +1,9 @@
 # store our keys here and the like
 
 MAPBOX_TOKEN = 'pk.eyJ1IjoibWluc3VuIiwiYSI6ImNqdjRqNGF3ZjJyaDYzeXMwcWpzOTE5ODkifQ.9DOLjPum-SBZ7kSUP264eQ'
+
+AWS_Username = 'Argo_User'
+Access key ID = 'AKIAXUTZXGO6ADPCJR6Y'
+Secret access key = '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'
+Console login link = 'https://525315027900.signin.aws.amazon.com/console'
+
