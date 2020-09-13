fd899b07357e9064b2187b1c482edde3aa92ae9e
minsun-ss <sungsharon@gmail.com>
2020-09-13 15:50:19 -0400

    Got a working iframe with dash.

--- app.py
+++ app.py
@@ -5,15 +5,14 @@ import plotly.graph_objs as go
 import json
 import pandas as pd
 
-df_html = pd.read_csv('data/test.csv').to_html()
-
+# Note that static assets such as html and the like must be served from the asset folders because Dash is pain
 app = dash.Dash(__name__)
 app.title = 'Ocean Temperature and Salinity in the Estuary and Gulf of St. Lawrence'
 server = app.server
 
 app.layout = html.Div([
     html.H1("Some Stuff")
-    #, html.Iframe(id='map', src=open('data/test.html').read(), width='100%', height='600')
+    , html.Iframe(id='map', src=app.get_asset_url('test.html'), width='100%', height='600')
 ])
 
 if __name__== '__main__':
