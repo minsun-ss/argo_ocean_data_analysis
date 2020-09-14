b4d38267a19d40db23968baacef05d9582d1d5bc
CICarlier <35584629+CICarlier@users.noreply.github.com>
2020-09-13 22:59:02 -0400

    updated credentials

--- helper/config.py
+++ helper/config.py
@@ -3,6 +3,6 @@
 MAPBOX_TOKEN = 'pk.eyJ1IjoibWluc3VuIiwiYSI6ImNqdjRqNGF3ZjJyaDYzeXMwcWpzOTE5ODkifQ.9DOLjPum-SBZ7kSUP264eQ'
 
 RDS_username = 'argo'
-RDS_pword = 'cLeHhyZHBYZPLzBNJhjg'
+RDS_pword = 'SmRFQpsEZSU7unv'
 RDS_endpoint = 'oceandb.cvgjf8n2qn4t.us-east-2.rds.amazonaws.com'
 RDS_port = 5432
