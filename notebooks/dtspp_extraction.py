404420e84801b12e72027cce2b29061bd2d40fab
minsun-ss <sungsharon@gmail.com>
2020-09-19 01:12:51 -0400

    Boatload of changes. Wrote extraction of files from ftp, extracting files from tar, deleting said files from folder (these files are quite large), and reading data into netcdf4 to find points in gulf.

--- notebooks/dtspp_extraction.py
+++ notebooks/dtspp_extraction.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import netCDF4
 from ftplib import FTP
+import os
+import tarfile
+import shutil
+from shapely.geometry import shape, Point
 
 def get_data(year):
     # extracts the tarzips from ftp for a specific year and dumps them into the data folder
@@ -28,9 +32,51 @@ def get_data(year):
     except:
         raise
 
+def extract_to_folder(location):
+    # extracts tgz file to specified location
+    file_list = [i for i in os.listdir('../data/gtspp') if 'tgz' in i]
+
+    # right now only unzips 1 file at a time
+    try:
+        for i in file_list[:1]:
+            print(i)
+            gtspp = tarfile.open(f'../data/gtspp/{i}')
+            gtspp.extractall(location)
+        print('Done.')
+    except:
+        raise
+
+def find_gulf_data():
+    # start opening up the nc files and finding point data. Returns a dataframe with matching points
+    # and accompanying data, uncleaned.
+    r = shapefile.Reader('../assets/shapefile/iho.shp')
+    gulf = shape(r.shapes()[0])
+    print(shapes)
+
+    gstpp_directory = '../data/gtspp/atlantic/2009/01'
+    latitude, longitude = [], []
+    for file in os.listdir(gstpp_directory):
+        cdf_coordinates = netCDF4.Dataset(f'{gstpp_directory}/{file}')
+
+        # first check to see if the point is within the area
+        array_lat, array_long = cdf_coordinates.variables['latitude'][:], cdf_coordinates.variables['longitude'][:]
+        lat = array_lat[array_lat.mask == False].data[0].tolist()[0]
+        long = array_long[array_long.mask == False].data[0].tolist()[0]
+        print(long, lat)
+        point = Point(long, lat)
+
+        if gulf.contains(point):
+            print(lat, long)
+            break
+
+def delete_folder_contents(location):
+    # to remove files
+    # location is usually '../data/gtspp/atlantic'
+    shutil.rmtree(location)
+
 def run_process():
     for i in range(2011, 2019):
         # grabbing data
         get_data(i)
 
-run_process()
+extract_to_folder('../data/gtspp')
