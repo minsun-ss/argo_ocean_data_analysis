8ff259f73b82e989f1b55887723d4972cb4696db
minsun-ss <sungsharon@gmail.com>
2020-10-28 20:40:20 -0400

    stage 1 - switching off rds

--- generate_data.py
+++ generate_data.py
@@ -16,42 +16,43 @@ def get_fish_info():
     return fish_info
 
 def get_fish_aggregate(**kwargs):
-    query = """
-    SELECT YEAR, COALESCE(fish_type, 'total') AS fish_type, avg_depth, fish_total FROM 
-    (SELECT EXTRACT(YEAR FROM DATE) AS YEAR, fish_type, avg(DEPTH) AS avg_depth, sum(fish_count) AS fish_total
-    FROM fish_data
-    WHERE fish_count>0
-    GROUP by YEAR, rollup(fish_type)) AS t1"""
+    # query = """
+    # SELECT YEAR, COALESCE(fish_type, 'total') AS fish_type, avg_depth, fish_total FROM
+    # (SELECT EXTRACT(YEAR FROM DATE) AS YEAR, fish_type, avg(DEPTH) AS avg_depth, sum(fish_count) AS fish_total
+    # FROM fish_data
+    # WHERE fish_count>0
+    # GROUP by YEAR, rollup(fish_type)) AS t1"""
 
-    try:
-        fish_data = db.run_query(query)
-    except:
-        fish_data = pd.read_csv('testfiles/fish_aggregate.csv')
+    # try:
+    #     fish_data = db.run_query(query)
+    #     pass
+    # except:
+    fish_data = pd.read_csv('testfiles/fish_aggregate.csv')
 
     # test aggregate file for now
     fish_data['year'] = fish_data['year'].astype('int')
     return fish_data
 
 def get_fish_locations():
-    query = """
-    SELECT extract(year from DATE) AS year, longitude, latitude, fish_type 
-    FROM fish_data
-    WHERE fish_count>0;
-    """
-
-    try:
-        fish_locations = db.run_query(fish_data)
-    except:
-        fish_locations = pd.read_csv('testfiles/fish_locations.csv')
+    # query = """
+    # SELECT extract(year from DATE) AS year, longitude, latitude, fish_type
+    # FROM fish_data
+    # WHERE fish_count>0;
+    # """
+    #
+    # try:
+    #     fish_locations = db.run_query(fish_data)
+    # except:
+    fish_locations = pd.read_csv('testfiles/fish_locations.csv')
 
     fish_locations['year'] = fish_locations['year'].astype('int')
     return fish_locations
 
 def get_param_data():
-    try:
-        param_data = build_param_data()
-    except:
-        param_data = pd.read_csv('testfiles/sample_param_data.csv')
+    # try:
+    #     param_data = build_param_data()
+    # except:
+    param_data = pd.read_csv('testfiles/sample_param_data.csv')
     return param_data
 
 def correlation_table(fish_df, param_df):
@@ -74,23 +75,24 @@ def build_param_data():
 
     # The regex will remove brackets and parentheses from the depth column and avoid (0, 100], [0, 100) issues.
     # Include 'AND in_gulf = 1' to the filter when the gstpp data is all added.
-    query = """
-    SELECT
-    extract(year from data_date) as year,
-    REGEXP_REPLACE(depth, '[\[\]\(\)]', '', 'g') as depth_range,
-    ROUND(AVG(temperature), 3) as temperature,
-    ROUND(AVG(salinity), 3) as salinity
-    FROM OCEAN_DATA
-    WHERE data_date BETWEEN '2009-01-01' AND '2018-12-31'
-    AND salinity BETWEEN 30 and 41
-    AND temperature BETWEEN -2.5 and 40
-    AND to_char(data_date,'Mon') in ('Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep')
-    AND depth <> 'nan'
-    AND in_gulf = 1
-    GROUP BY year, depth_range
-    ORDER BY year, temperature;
-    """
-    param_df = db.run_query(query)
+    # query = """
+    # SELECT
+    # extract(year from data_date) as year,
+    # REGEXP_REPLACE(depth, '[\[\]\(\)]', '', 'g') as depth_range,
+    # ROUND(AVG(temperature), 3) as temperature,
+    # ROUND(AVG(salinity), 3) as salinity
+    # FROM OCEAN_DATA
+    # WHERE data_date BETWEEN '2009-01-01' AND '2018-12-31'
+    # AND salinity BETWEEN 30 and 41
+    # AND temperature BETWEEN -2.5 and 40
+    # AND to_char(data_date,'Mon') in ('Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep')
+    # AND depth <> 'nan'
+    # AND in_gulf = 1
+    # GROUP BY year, depth_range
+    # ORDER BY year, temperature;
+    # """
+    # param_df = db.run_query(query)
+    param_df = pd.read_csv('testfiles/param_grid.csv')
 
     # Turn the depth_range column values back into categories. Start by replacing commas by hyphen and removing the white space.
     param_df['depth_range'] = param_df['depth_range'].str.replace(r"\, ", "-")
